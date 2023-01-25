ea149c283e193ae0290b7e50d0055b50604ffff4
Kurt Jensen <kajensen@users.noreply.github.com>
2023-01-25 15:28:58 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,19 +5,19 @@ setup(
     'gobiko',
     'gobiko.apns'
   ],
-  version = '0.1.6',
+  version = '0.1.7',
   description = 'A library for interacting with APNs using HTTP/2 and token-based authentication.',
   author = 'Gene Sluder',
   author_email = 'gene@gobiko.com',
-  url = 'https://github.com/genesluder/python-apns',
-  download_url = 'https://github.com/genesluder/python-apns/tarball/0.1.6',
+  url = 'https://github.com/kajensen/python-apns',
+  download_url = 'https://github.com/kajensen/python-apns/tarball/0.1.7',
   keywords = [
     'apns', 
     'push notifications',
   ],
   classifiers = [],
   install_requires=[
-    'cryptography<=3.3.2',
+    'cryptography>=39.0.0',
     'hyper',
     'pyjwt',
   ],
